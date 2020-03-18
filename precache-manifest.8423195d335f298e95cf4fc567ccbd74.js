9c1854c85afc02a1e9d32cc7353eb82c85c9b4ef
Stefan Michetschlager <stefan.michetschlager@bitfactor.fi>
2020-03-18 11:26:10 +0200

    Updates

---
+++ precache-manifest.8423195d335f298e95cf4fc567ccbd74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b0d89e80eebf72cc30ceb4d741e223",
+    "url": "/math-game/index.html"
+  },
+  {
+    "revision": "074c9ac924f8d9a3739b",
+    "url": "/math-game/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4ac94fc88002392ff8ba",
+    "url": "/math-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "074c9ac924f8d9a3739b",
+    "url": "/math-game/static/js/2.9851869a.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/math-game/static/js/2.9851869a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac94fc88002392ff8ba",
+    "url": "/math-game/static/js/main.62b13994.chunk.js"
+  },
+  {
+    "revision": "68b202a7cd29ef1365db",
+    "url": "/math-game/static/js/runtime-main.ec919485.js"
+  }
+]);
